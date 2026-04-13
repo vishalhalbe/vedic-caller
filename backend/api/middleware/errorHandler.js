0503f6ddec7aa3383a388db213b804dc5836acde
vishalhalbe <vishalhalbe@users.noreply.github.com>
2026-04-13 16:35:34 +0530

    Add global error handler

---
+++ backend/api/middleware/errorHandler.js
@@ -0,0 +1,8 @@
+module.exports = (err, req, res, next) => {
+  console.error(err);
+
+  res.status(err.status || 500).json({
+    error: true,
+    message: err.message || 'Internal Server Error'
+  });
+};
